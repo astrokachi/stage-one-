f5cbe871e8a579a494dabe0f7def135993f9e5f7
Kachi <astrokachi@gmail.com>
2022-10-24 13:36:12 +0100

    Added data file

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const links = [
+  {
+    name: 'Twitter Link',
+    link: 'https://twitter.com/kachi_oz',
+  },
+  {
+    name: 'Zuri Team',
+    link: 'https://training.zuri.team/',
+    id: 'btn__zuri',
+  },
+  {
+    name: 'Zuri Books',
+    link: 'http://books.zuri.team',
+    id: 'books',
+  },
+  {
+    name: 'Python Books',
+    link: 'https://books.zuri.team/python-for-beginners?ref_id=Ozoemena%Kachi',
+    id: 'book__python',
+  },
+  {
+    name: 'Background Check for Coders',
+    link: 'https://background.zuri.team',
+    id: 'pitch',
+  },
+  {
+    name: 'Design Books',
+    link: 'https://books.zuri.team/design-rules ',
+    id: 'book__design',
+  },
+];
+
+export default links;
