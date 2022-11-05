3b0d4fb112dc3478c75d5b3626d17342a7df872c
Kachi <astrokachi@gmail.com>
2022-11-05 02:20:46 +0100

    added contact route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
